56af9f3e0b87e9b1e2536d76e94f9085ff01e1f3
Aniketyadav77 <aniketyadavdv07@gmail.com>
2024-07-27 00:40:27 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://images.unsplash.com/photo-1601134467661-3d775b999c8b?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  }
+}
